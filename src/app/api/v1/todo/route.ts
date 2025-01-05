199ded9487cc56e595181e4e2130b91e270ed35e
HMaroba <marobahlalele910@gmail.com>
2025-01-05 19:36:32 +0200

    Modofied api

--- src/app/api/v1/todo/route.ts
+++ src/app/api/v1/todo/route.ts
@@ -25,17 +25,33 @@ export async function POST(req : NextRequest) {
     }
 }
 
-export async function GET() {
+export async function GET(req :NextRequest) {
+    
     const ITEM_PER_PAGE = 2;
+    const {searchParams } = new URL(req.url);
+    const page = parseInt(searchParams.get('page') || '1');
+    const search = searchParams.get('search') || ""
   try {
+    
     const todos = await prisma.todo.findMany({
+        where : {
+            title: {
+                contains : search,
+            }
+        },
         orderBy: {createdAt : 'desc'},
         take: ITEM_PER_PAGE,
+        skip: (page - 1) * ITEM_PER_PAGE,
     });
+    const totalItems = await prisma.todo.count();
+
+    const totalPages = Math.ceil(totalItems / ITEM_PER_PAGE);
 
     return NextResponse.json({
         status : 200,
         todos : todos,
+        currentPage: page,
+        totalPages,
     })
   } catch (error) {
     return NextResponse.json({
